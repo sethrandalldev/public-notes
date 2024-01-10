34397e5e4b9101eb77227b300fcda447995e4586
Seth Randall <sethjrandall@gmail.com>
2024-01-09 22:34:01 -0500

    Add notes schema and notes seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,4 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Note.create(title: "First Note", content: "These notes are anonymous.")
